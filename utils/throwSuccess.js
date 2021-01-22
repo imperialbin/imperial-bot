8e8c56376071d620aa370dff48e75bae20a616d6
devlooskie <hello@looskie.com>
2021-01-22 17:02:13 -0500

    Fix throwSuccess utility

--- utils/throwSuccess.js
+++ utils/throwSuccess.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
 
-module.exports = (msg, errMessage) => {
-  msg.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(message).setColor(0x4fee54))
+module.exports = (msg, successMessage) => {
+  msg.channel.send(new Discord.MessageEmbed().setTitle('Success!').setDescription(successMessage).setColor(0x4fee54))
 }
