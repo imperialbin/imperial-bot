831a39edd2f917cb0852504c40a92f3851abe42c
devlooskie <hello@looskie.com>
2021-01-22 17:13:12 -0500

    help command

--- modules/help.js
+++ modules/help.js
@@ -1,3 +1,9 @@
+const Discord = require('discord.js');
+
 module.exports = msg => {
-  msg.channel.send('Help!');
+  msg.channel.send(new Discord.MessageEmbed().setTitle('Commands').setDescription('All the commands inside IMPERIAL').setColor('#24292e').addFields(
+    { name: '!imp post', value: 'After initiating, it will prompt you to paste the code you want to save in a paste' },
+    { name: '!imp api', value: 'Sends you a DM so you can hook your API token to your Discord account' },
+    { name: '!imp getcode', value: 'Returns code from desired paste' }
+  ));
 }
