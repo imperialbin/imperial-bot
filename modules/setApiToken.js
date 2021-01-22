9dc984e6095f7e44717f2fa0a4c8abe22d6ddaee
devlooskie <hello@looskie.com>
2021-01-22 17:01:18 -0500

    use the utilities

--- modules/setApiToken.js
+++ modules/setApiToken.js
@@ -1,30 +1,33 @@
 const Users = require('../models/User');
 const fetch = require('node-fetch')
 
+// Utilities
+const throwError = require('../utils/throwError');
+const throwSuccess = require('../utils/throwSuccess');
+
 module.exports = async (msg, client) => {
   if (msg.channel.type == 'dm') {
     if (msg.content.includes('IMPERIAL-')) {
       Users.findOne({ userId: msg.author.id }, async (err, user) => {
         if (user) {
-          msg.channel.send('You already have an API token hooked up to your discord account!')
+          throwError(msg, 'You already have an API token hooked up to your discord account!')
         } else {
           const req = await fetch(`https://imperialb.in/api/checkApiToken/${msg.content}`);
           const verifyToken = await req.json();
           if (verifyToken.success) {
-            msg.channel.send('API token is valid!')
             const newUser = new Users({
               userId: msg.author.id,
               apiToken: msg.content
             })
             newUser.save()
-              .then(() => msg.channel.send('Successfully saved your api token!'))
+              .then(() => throwSuccess(msg, 'Successfully set your API token to your Discord account!'))
           } else {
-            msg.channel.send('API token is not valid!')
+            throwError(msg, 'That API token isn\'t valid!')
           }
         }
       })
     } else {
-      msg.channel.send('Please insert a valid API token!')
+      throwError(msg, 'Please insert a valid API token!')
     }
   } else {
     client.users.cache.get(msg.author.id).send('Please reply to this DM with a valid API token! \n\n Ex: `IMPERIAL-xxxx-xxx-xxxx`');
