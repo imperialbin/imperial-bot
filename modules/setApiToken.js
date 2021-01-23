a580b72199d475c4366d2447f3430c7689c9a53a
devlooskie <hello@looskie.com>
2021-01-22 19:06:10 -0500

    Allow changing of API keys

--- modules/setApiToken.js
+++ modules/setApiToken.js
@@ -7,10 +7,32 @@ const throwSuccess = require('../utils/throwSuccess');
 
 module.exports = async (msg, client) => {
   if (msg.channel.type == 'dm') {
-    if (msg.content.includes('IMPERIAL-')) {
+    if (msg.content.includes('change')) {
+      if (msg.content.split(' ').length == 2) {
+        const req = await fetch(`https://imperialb.in/api/checkApiToken/${msg.content.split(' ')[1]}`);
+        const verifyToken = await req.json();
+        if (verifyToken.success) {
+          Users.findOneAndUpdate({ userId: msg.author.id }, { $set: { apiToken: msg.content.split(' ')[1] } }, (err, user) => {
+            if (err) {
+              console.log(err);
+              return throwError(msg, 'A server error occured whilst trying to change your API key!')
+            }
+            if (user) {
+              return throwSuccess(msg, 'Successfully set your API token to your Discord account!');
+            } else {
+              return throwError(msg, 'You have not linked you\'re account yet! Please just pass your API key!')
+            }
+          })
+        } else {
+          return throwError(msg, 'That API token isn\'t valid!')
+        }
+      } else {
+        return throwError(msg, 'You must pass your API key as well! Do change IMPERIAL-xxxx-xxxx-xxx')
+      }
+    } else if (msg.content.includes('IMPERIAL-')) {
       Users.findOne({ userId: msg.author.id }, async (err, user) => {
         if (user) {
-          throwError(msg, 'You already have an API token hooked up to your discord account!')
+          throwError(msg, 'You already have an API token hooked up to your discord account! Do you want to relink? Type \'change IMPERIAL-xxxx-xxxx-xxxx\'');
         } else {
           const req = await fetch(`https://imperialb.in/api/checkApiToken/${msg.content}`);
           const verifyToken = await req.json();
