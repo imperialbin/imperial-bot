c48af8366865ef335a89d3169844efae4458caa7
devlooskie <hello@looskie.com>
2021-01-24 12:20:17 -0500

    Seperate this to make it more readable

--- modules/postCode.js
+++ modules/postCode.js
@@ -3,26 +3,28 @@ const Imperial = require('imperial-node').Imperial;
 
 // Utilities
 const throwError = require('../utils/throwError');
+const options = {
+  max: 1,
+  time: 30000
+}
 
 module.exports = msg => {
   Users.findOne({ userId: msg.author.id }, (err, user) => {
     if (user) {
       msg.channel.send('Go ahead and paste what you want to save in the chat, or type \'CANCEL\' in all caps to cancel!')
-      msg.channel.awaitMessages(awaitingMessage => awaitingMessage.author.id == msg.author.id, {
-        max: 1,
-        time: 30000
-      }).then(authorsMessage => {
-        if (authorsMessage.first()) {
-          const api = new Imperial(user.apiToken)
-          api.postCode(authorsMessage.first().content)
-            .then(paste => msg.reply(paste.formattedLink))
-            .then(msg.delete());
-        } else {
-          throwError(msg, 'You didn\'t respond in 30 seconds! The operation has been cancelled')
-        }
-      }).catch(err => {
-        throwError(msg, 'An internal server error occured!')
-      })
+      msg.channel.awaitMessages(awaitingMessage => awaitingMessage.author.id == msg.author.id, options)
+        .then(authorsMessage => {
+          if (authorsMessage.first()) {
+            const api = new Imperial(user.apiToken)
+            api.postCode(authorsMessage.first().content)
+              .then(paste => msg.reply(paste.formattedLink))
+              .then(msg.delete());
+          } else {
+            throwError(msg, 'You didn\'t respond in 30 seconds! The operation has been cancelled')
+          }
+        }).catch(() => {
+          throwError(msg, 'An internal server error occured!')
+        })
     } else {
       throwError(msg, 'Please hook up your IMPERIAL account by doing `!imp api` before trying to post code!')
     }
