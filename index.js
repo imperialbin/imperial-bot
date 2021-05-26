fc066a710d59ac48cad4025d8ed2bc5fb895a70b
looskie <50378828+Looskie@users.noreply.github.com>
2021-05-26 11:41:10 -0400

    i hate javascript

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '!imp';
-const mongoose = require('mongoose');
-require('dotenv').config();
+const prefix = "!imp";
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Modules
-const postCode = require('./modules/postCode');
-const saveMessages = require('./modules/saveMessages');
-const getCode = require('./modules/getCode');
-const setApiToken = require('./modules/setApiToken');
-const help = require('./modules/help');
+const postCode = require("./modules/postCode");
+const saveMessages = require("./modules/saveMessages");
+const getCode = require("./modules/getCode");
+const setApiToken = require("./modules/setApiToken");
+const help = require("./modules/help");
 
 // Utilities
-const throwError = require('./utils/throwError');
+const throwError = require("./utils/throwError");
 
-client.on('ready', () => {
-  client.user.setActivity('!imp help | https://imperialb.in/', { type: 'PLAYING' });
-  console.log('READY');
-  mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@discordusers.hy2f3.mongodb.net/users?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    if (err) return console.log(err);
-    console.log('CONNECTED DATABASE');
-  })
-})
+client.on("ready", () => {
+  client.user.setActivity("!imp help | https://imperialb.in/", {
+    type: "PLAYING",
+  });
+  console.log("READY");
+  mongoose.connect(
+    process.env.DB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+      if (err) return console.log(err);
+      console.log("CONNECTED DATABASE");
+    }
+  );
+});
 
-client.on('message', async msg => {
-  if (msg.channel.type == 'dm' && !msg.author.bot) setApiToken(msg, client);
+client.on("message", async (msg) => {
+  if (msg.channel.type == "dm" && !msg.author.bot) setApiToken(msg, client);
   if (msg.author.bot) return;
   if (msg.content.indexOf(prefix) !== 0) return;
   const command = msg.content
@@ -34,32 +40,43 @@ client.on('message', async msg => {
     .shift()
     .toLowerCase();
   switch (true) {
-    case (command === 'help' || command === 'h'):
+    case command === "help" || command === "h":
       help(msg);
       break;
-    case (command === 'paste' || command === 'postcode' || command === 'post' || command === 'p'):
+    case command === "paste" ||
+      command === "postcode" ||
+      command === "post" ||
+      command === "p":
       postCode(msg);
       break;
-    case (command === 'save' || command === 'savemessages' || command === 'savemessages' || command === 'sm'):
+    case command === "save" ||
+      command === "savemessages" ||
+      command === "savemessages" ||
+      command === "sm":
       saveMessages(msg);
       break;
-    case (command === 'getcode' || command === 'get' || command === 'code' || command === 'g'):
+    case command === "getcode" ||
+      command === "get" ||
+      command === "code" ||
+      command === "g":
       getCode(msg);
       break;
-    case (command === 'setapi' || command === 'setapitoken' || command === 'api' || command === 'setup'):
+    case command === "setapi" ||
+      command === "setapitoken" ||
+      command === "api" ||
+      command === "setup":
       setApiToken(msg);
       break;
     default:
-      throwError(msg, 'Unknown command!')
+      throwError(msg, "Unknown command!");
       break;
   }
-})
+});
 
-
-if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
-  client.login(process.env.BOT_TOKEN_DEV)
-  console.log('DEV TOKEN BEING USED!')
+if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+  client.login(process.env.BOT_TOKEN_DEV);
+  console.log("DEV TOKEN BEING USED!");
 } else {
-  client.login(process.env.BOT_TOKEN)
-  console.log('PROD TOKEN BEING USED!')
+  client.login(process.env.BOT_TOKEN);
+  console.log("PROD TOKEN BEING USED!");
 }
--- modules/getCode.js
+++ modules/getCode.js
@@ -1,27 +1,46 @@
-const Users = require('../models/User');
-const Imperial = require('imperial-node').Imperial;
+const Users = require("../models/User");
+const Imperial = require("imperial-node").Imperial;
 
 // Utilities
-const throwError = require('../utils/throwError');
+const throwError = require("../utils/throwError");
 
-module.exports = msg => {
+module.exports = (msg) => {
   Users.findOne({ userId: msg.author.id }, (err, user) => {
-    if (err) return throwError(msg, 'An internal server error occurred! Please contact an admin!');
+    if (err)
+      return throwError(
+        msg,
+        "An internal server error occurred! Please contact an admin!"
+      );
     if (user) {
       const api = new Imperial(user.apiToken);
-      const documentId = msg.content.split(' ')[2]
-      api.getCode(documentId)
-        .then(paste => {
-          if (paste.success) {
+      const documentId = msg.content.split(" ")[2];
+      api
+        .getDocument(documentId)
+        .then((document) => {
+          if (document) {
             msg.channel
-              .send(`\`\`\`${paste.document}\`\`\``)
-              .catch(() => throwError(msg, `Code is too long! Here is a URL, https://imperialb.in/p/${documentId}`));
+              .send(`\`\`\`${document.content}\`\`\``)
+              .catch(() =>
+                throwError(
+                  msg,
+                  `Code is too long! Here is a URL, https://imperialb.in/p/${documentId}`
+                )
+              );
           } else {
-            throwError(msg, 'Sorry, but we couldn\'t find the paste you were looking for!')
+            throwError(
+              msg,
+              "Sorry, but we couldn't find the paste you were looking for!"
+            );
           }
-        }).catch(() => throwError(msg, 'There was an error getting your paste!'))
+        })
+        .catch(() =>
+          throwError(msg, "There was an error getting your document!")
+        );
     } else {
-      throwError(msg, 'Please link your IMPERIAL account by doing `!imp api` before trying to get code!')
+      throwError(
+        msg,
+        "Please link your IMPERIAL account by doing `!imp api` before trying to get code!"
+      );
     }
-  })
-}
+  });
+};
