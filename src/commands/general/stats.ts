74e42e5bac2ffc0a0df525378fbfd8b1a9dd8b32
Bereket Semagn <semagnbereket@gmail.com>
2021-10-11 16:57:09 -0400

    add statistics command

---
+++ src/commands/general/stats.ts
@@ -0,0 +1,32 @@
+import { Command, CommandOptions } from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import type { Message } from "discord.js";
+import { sendEmbed } from "../../lib/sendEmbed";
+
+@ApplyOptions<CommandOptions>({
+  description: "Get statistics about the bot",
+})
+export class StatsCommand extends Command {
+  public async run(message: Message) {
+    const embed = sendEmbed(
+      "Bot Statistics",
+      `Below are stats on the Imperial Bot`,
+      message,
+      false,
+      [
+        {
+          name: "Guilds",
+          value: `${this.container.client.guilds.cache.size}`,
+          inline: true,
+        },
+        {
+          name: "Users",
+          value: `${this.container.client.users.cache.size}`,
+          inline: true,
+        },
+      ]
+    );
+
+    return message.channel.send({ embeds: [embed] });
+  }
+}
