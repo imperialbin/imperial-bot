bbcacc0faa84f7178259cad42e10f9899fb7e85d
Bereket Semagn <semagnbereket@gmail.com>
2021-10-11 20:26:29 -0400

    make mention event return an embed

--- src/listeners/mentionPrefixOnly.ts
+++ src/listeners/mentionPrefixOnly.ts
@@ -1,9 +1,18 @@
-import { Listener } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import { Listener } from "@sapphire/framework";
+import type { Message } from "discord.js";
+import { sendEmbed } from "../lib/sendEmbed";
 
-export class MentionEvent extends Listener<'mentionPrefixOnly'> {
-	public async run(message: Message) {
-		const prefix = this.container.client.options.defaultPrefix;
-		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'You do not need a prefix in DMs.');
-	}
+export class MentionEvent extends Listener<"mentionPrefixOnly"> {
+  public async run(message: Message) {
+    const prefix = this.container.client.options.defaultPrefix;
+    const embed = sendEmbed(
+      "Prefix",
+      prefix
+        ? `My prefix in this guild is: \`${prefix}\``
+        : "You do not need a prefix in DMs.",
+      message,
+      false
+    );
+    return message.reply({ embeds: [embed] });
+  }
 }
